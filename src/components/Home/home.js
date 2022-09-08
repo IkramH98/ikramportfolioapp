9eaf9bb2cc3dc6ea3c242ee24623bf19da4415e0
ikram <ikranidil@hotmail.co.uk>
2022-09-08 18:41:39 +0100

    working locally

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { Typer } from "react-typetool";
+// import { Typer } from "react-typetool";
 // import Typed from "react-typed"
-import Particles from "react-tsparticles";
-import loadFull  from "react-tsparticles";
-import { useCallback } from "react";
+// import Particles from "react-tsparticles";
+// import loadFull  from "react-tsparticles";
+// import { useCallback } from "react";
 import "./home.css"
 
 import pic from "../../photos/image.png";
 
 const Home = () => {
-  const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-}, []);
+//   const particlesInit = useCallback(async (engine) => {
+//     console.log(engine);
+//     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+//     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+//     // starting from v2 you can add only the features you need reducing the bundle size
+//     await loadFull(engine);
+// }, []);
 
-const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
-}, []);
+// const particlesLoaded = useCallback(async (container) => {
+//     await console.log(container);
+// }, []);
 
   return (
     <div className="w-full relative min-h-screen pt-20">
@@ -27,7 +27,7 @@ const particlesLoaded = useCallback(async (container) => {
         <div className="container overflow-hidden">
           <div className="particles-canvas-el ">
 
-          <Particles id="tsparticles" url="http://foo.bar/particles.json" init={particlesInit} loaded={particlesLoaded} />
+          {/* <Particles id="tsparticles" url="http://foo.bar/particles.json" init={particlesInit} loaded={particlesLoaded} /> */}
 {/* {      
             <Particles
     params={{
